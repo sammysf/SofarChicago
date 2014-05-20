91ff405ac88d11d066743ef15ab141c00d7cd610
sammysf <samueltoizer2014@u.northwestern.edu>
2014-05-20 12:14:47 -0500

    Removed email password from actual code

--- sofarchicago.py
+++ sofarchicago.py
@@ -22,7 +22,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'samtoizer'
-app.config['MAIL_PASSWORD'] = 'isitred!'
+app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
 
 mail = Mail(app)
 db = SQLAlchemy(app)
